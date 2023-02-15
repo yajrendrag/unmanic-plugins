3bfd7fa79944d1d43df85e57f125af83d14a478e
yajrendrag <jay@gardner.us.com>
2023-02-14 22:13:59 -0700

    fix lib.ffmpeg path

--- source/limit_library_search_by_metadata/plugin.py
+++ source/limit_library_search_by_metadata/plugin.py
@@ -26,7 +26,7 @@ import os
 
 from unmanic.libs.unplugins.settings import PluginSettings
 
-from lib.ffmpeg import Probe
+from source.limit_library_search_by_metadata.lib.ffmpeg import Probe
 
 # Configure plugin logger
 logger = logging.getLogger("Unmanic.Plugin.limit_library_search_by_metadata")
