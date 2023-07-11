e2c2e603121dbd0e234d9f1ac063b42e4461c43b
yajrendrag <jay@gardner.us.com>
2023-07-10 19:42:48 -0600

    [force_processing] initial release

---
+++ source/force_processing/plugin.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    plugins.__init__.py
+
+    Written by:               yajrendrag <yajdude@gmail.com>
+    Date:                     10 July 2023, (19:14 PM)
+
+    Copyright:
+        Copyright (C) 2023 Jay Gardner
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
+import logging
+import os
+
+from unmanic.libs.unplugins.settings import PluginSettings
+
+from force_processing.lib.ffmpeg import Probe, Parser
+
+# Configure plugin logger
+logger = logging.getLogger("Unmanic.Plugin.force_processing")
+
+class Settings(PluginSettings):
+    settings = {}
+
+
+def on_library_management_file_test(data):
+    """
+    Runner function - enables additional actions during the library management file tests.
+
+    The 'data' object argument includes:
+        path                            - String containing the full path to the file being tested.
+        issues                          - List of currently found issues for not processing the file.
+        add_file_to_pending_tasks       - Boolean, is the file currently marked to be added to the queue for processing.
+
+    :param data:
+    :return:
+
+    """
+    # Get the path to the file
+    abspath = data.get('path')
+
+    # Get file probe
+    probe_data = Probe(logger, allowed_mimetypes=['audio', 'video'])
+
+    # Get stream data from probe
+    if probe_data.file(abspath):
+        probe_streams = probe_data.get_probe()["streams"]
+        probe_format = probe_data.get_probe()["format"]
+        data['add_file_to_pending_tasks'] = True
+    else:
+        logger.info("Probe data failed - Blocking everything.")
+        data['add_file_to_pending_tasks'] = False
+
+    logger.debug("Queue status is: {}".format(data['add_file_to_pending_tasks']))
+    return data
