805eb904e6841a8d171f03069a3a622b6a853d32
yajrendrag <jay@gardner.us.com>
2023-11-11 16:18:56 -0700

    moved srt deletion to post processor routine

--- source/add_srt_to_video/changelog.md
+++ source/add_srt_to_video/changelog.md
@@ -1,4 +1,7 @@
 
+**<span style="color:#56adda">0.0.4</span>**
+- moved the deletion of the srt files to a post processing routine in order to test task success before deletion
+
 **<span style="color:#56adda">0.0.3</span>**
 - fix lang3 to isolate a single 3 letter code when a two letter code translates to more than 1 code
 - fix 'no' in lang_codes to add selection asterisk for one of the 3 letter codes
--- source/add_srt_to_video/plugin.py
+++ source/add_srt_to_video/plugin.py
@@ -199,3 +199,38 @@ def on_worker_process(data):
                 os.remove(srt_file)
                 logger.info("srt file '{}' has been added to video file of the same basename; the srt file has been deleted.".format(srt_file))
     return data
+
+def on_postprocessor_task_results(data):
+    """
+    Runner function - provides a means for additional postprocessor functions based on the task success.
+
+    The 'data' object argument includes:
+        final_cache_path                - The path to the final cache file that was then used as the source for all destination files.
+        library_id                      - The library that the current task is associated with.
+        task_processing_success         - Boolean, did all task processes complete successfully.
+        file_move_processes_success     - Boolean, did all postprocessor movement tasks complete successfully.
+        destination_files               - List containing all file paths created by postprocessor file movements.
+        source_data                     - Dictionary containing data pertaining to the original source file.
+
+    :param data:
+    :return:
+
+    """
+
+    if data.get('library_id'):
+        settings = Settings(library_id=data.get('library_id'))
+    else:
+        settings = Settings()
+
+    status = data.get('task_processing_success')
+    logger.debug("status: '{}'".format(status))
+
+    if status:
+        abspath = data.get('source_data').get('abspath')
+        basefile = os.path.splitext(abspath)[0]
+        for j in range(len(glob.glob(basefile+'*.*[a-z].srt'))):
+            srt_file = glob.glob(basefile+'*.*[a-z].srt')[j]
+            os.remove(srt_file)
+            logger.info("srt file '{}' has been added to video file of the same basename; the srt file has been deleted.".format(srt_file))
+
+    return data
