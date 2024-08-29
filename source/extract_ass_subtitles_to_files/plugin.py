b359414e4de07bd15f137b79db8038c911c1512e
soultaco83 <76927195+soultaco83@users.noreply.github.com>
2024-08-28 22:52:05 -0400

    Update plugin.py

--- source/extract_ass_subtitles_to_files/plugin.py
+++ source/extract_ass_subtitles_to_files/plugin.py
@@ -4,7 +4,18 @@
 """
     Written by:               Modified by Soultaco83 (Forked from Josh.5 Extract SRT)
     Date:                     18 April 2021, (1:41 AM)
+    Copyright:
+        Copyright (C) 2021 Josh Sunnex
 
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
 """
 import logging
 import os
