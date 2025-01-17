65c62296df9ca7c629b7b16be9994fde33f3128e
yajrendrag <jay@gardner.us.com>
2025-01-17 15:17:19 -0700

    install script updates

--- source/split_mkv/init.d/install_helper_modules.sh
+++ source/split_mkv/init.d/install_helper_modules.sh
@@ -8,9 +8,19 @@ if ! command -v mkvmerge &> /dev/null; then
     apt-get install mkvtoolnix -y
 else
     echo "**** mkvtoolnix already installed ****"
+fi
 
 if ! command -v scenedetect &> /dev/null; then
     echo "**** Installing pyscenedetect ****"
     python3 -m pip install --upgrade scenedetect[opencv-headless]
 else
     echo "**** pyscenedetect already installed ****"
+fi
+
+if [[ -d /config/.unmanic/userdata/split_mkv ]]; then
+    if [[ ! -f /config/.unmanic/userdata/split_mkv/credits_dictionary ]]; then
+        echo "**** moving credits_dictionary into place ****"
+        cp -a ./credits_dictionary  /config/.unmanic/userdata/split_mkv/credits_dictionary
+    else
+        cp -a ./credits_dictionary  /config/
+fi
