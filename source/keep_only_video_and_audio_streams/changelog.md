0a056d10a908f8b9c4364ea53e9761bf30b93197
yajrendrag <jay@gardner.us.com>
2025-12-16 16:39:35 -0700

    python_command variable

--- source/keep_only_video_and_audio_streams/changelog.md
+++ source/keep_only_video_and_audio_streams/changelog.md
@@ -1,4 +1,7 @@
 
+**<span style="color:#56adda">0.0.8</span>**
+- use a python_command variable to make init.d script work on venv and non-venv systems
+
 **<span style="color:#56adda">0.0.7</span>**
 - remove requirements.txt content
 - change to init.d based plugin installation
--- source/keep_only_video_and_audio_streams/init.d/install-ffsubsync.sh
+++ source/keep_only_video_and_audio_streams/init.d/install-ffsubsync.sh
@@ -2,9 +2,18 @@
 
 # Script is executed by the Unmanic container on startup to auto-install dependencies
 
+TARGET_DIR="/opt/venv"
+if [[ -f "$TARGET_DIR/pyvenv.cfg" && -f "$TARGET_DIR/bin/python3" ]]; then
+    # Venv case (Ubuntu 24 style or manual venv)
+    python_command="$TARGET_DIR/bin/python3"
+else
+    # System case (Ubuntu 22 style)
+    python_command="/usr/bin/python3"
+fi
+
 if ! command -v ffsubsync &> /dev/null; then
     echo "**** Installing ffsubsync ****"
-    python3 -m pip install ffsubsync
+    "$python_command" -m pip install ffsubsync
 else
     echo "**** ffsubsync already installed ****"
 fi
